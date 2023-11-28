f43fa76473215056b147cf390014732f5924e91c
shyun96 <75017813+shyun96@users.noreply.github.com>
2023-11-28 10:28:19 +0900

    Delete server directory

--- server/scripts/runServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd      /home/ubuntu/ssgbay
-
-echo    ">>> run app -------------------------------------------------------"
-
-cron
-
-python3 -u app.py > /dev/null 2> /dev/null < /dev/null &
